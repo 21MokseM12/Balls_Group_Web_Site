87e577588656f5c3b69153b3b15f1450d70acc75
moksem <maksim.buynov.00@mail.ru>
2024-12-06 16:49:51 +0300

    add preloader to employee page

--- src/main/resources/static/js/script_login.js
+++ src/main/resources/static/js/script_login.js
@@ -2,3 +2,15 @@ function closeModal(modalId) {
     const modal = document.getElementById(modalId);
     modal.style.display = "none";
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+    const preloader = document.getElementById('preloader');
+    const mainContent = document.getElementById('main-content');
+
+    setTimeout(() => {
+        preloader.classList.add('hidden');
+        mainContent.classList.remove('hidden');
+        mainContent.classList.add('show');
+        document.body.style.overflow = 'auto';
+    }, 2500);
+});
