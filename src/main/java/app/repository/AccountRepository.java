22ad825c73b5cca6d16f6ce89642518b1f1c96da
moksem <maksim.buynov.00@mail.ru>
2024-12-15 21:16:29 +0300

    fix: change inheritance of repository from Crud to Jpa

--- src/main/java/app/repository/AccountRepository.java
+++ src/main/java/app/repository/AccountRepository.java
@@ -1,11 +1,13 @@
 package app.repository;
 
-import app.domain.users.Account;
-import org.springframework.data.repository.CrudRepository;
+import app.domain.entites.users.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-public interface AccountRepository extends CrudRepository<Account, Long> {
+@Repository
+public interface AccountRepository extends JpaRepository<Account, Long> {
     Optional<Account> findAccountByUsername(String username);
     boolean existsByUsername(String username);
     boolean existsById(Long id);
