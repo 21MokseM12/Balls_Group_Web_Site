e1b2ca32219eca174a161b9d4a8bcf6b2d880bda
moksem <maksim.buynov.00@mail.ru>
2024-12-20 17:33:13 +0300

    feature: dynamic load products on main shop page

--- src/main/resources/static/js/script_shop_main_page.js
+++ src/main/resources/static/js/script_shop_main_page.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", () => {
+function initializeCarousel() {
     const carousel = document.querySelector(".carousel");
     const items = document.querySelectorAll(".carousel-item");
     const prevButton = document.querySelector(".carousel-btn.prev");
@@ -32,42 +32,81 @@ document.addEventListener("DOMContentLoaded", () => {
 
     // Устанавливаем стартовое положение
     updateCarousel();
-});
+}
+
+async function loadCategories() {
+    const categoryList = document.getElementById('category-list');
+    try {
+        const response = await fetch('/api/v1/edit-shop/get-all/categories/');
+        const categories = await response.json();
+
+        categoryList.innerHTML = ''; // Очищаем список перед загрузкой
+        if (categories.length === 0) {
+            categoryList.innerHTML = '<p>Категории появятся совсем скоро =)</p>';
+            return;
+        }
+
+        categories.forEach(category => {
+            const categoryItem = document.createElement('div');
+            categoryItem.classList.add('category-card'); // Класс для стилизации карточки
+
+            categoryItem.innerHTML = `
+                <div class="category-value">
+                    <a href="/main/shop/category-page/${category.id}">${category.category}</a>
+                </div>
+            `;
+            categoryList.appendChild(categoryItem);
+        });
+    } catch (error) {
+        console.error(error);
+        categoryList.innerHTML = '<p>Ошибка загрузки категорий. Попробуйте позже.</p>';
+    }
+}
+
+async function loadProducts() {
+    const productsList = document.getElementById('products-list');
+    try {
+        const response = await fetch(`/api/v1/edit-shop/get-all/products/`);
+        const products = await response.json();
+
+        productsList.innerHTML = '';
+        if (productsList.length === 0) {
+            productsList.innerHTML = `<p>Товары появятся совсем скоро =)</p>`;
+            return;
+        }
+
+        for (const product of products) {
+            const s3Response = await fetch(`/api/v1/s3bucket-storage/nikonshop-s3-database/download/${product.productPhotoLinks[0]}`);
+            const photo = await s3Response.blob();
+
+            const productItem = document.createElement('div');
+            productItem.classList.add('carousel-item'); // Класс для стилизации карточки
+
+            productItem.innerHTML = `
+                <img src="${URL.createObjectURL(photo)}">
+                <p>${product.title}</p>
+                <p class="price">${product.price} ₽</p>
+                <div class="button-container">
+                  <button class="add-to-cart-btn">Добавить в корзину</button>
+                  <a class="about-btn" href="/main/shop/product-page/${product.id}">Подробнее...</a>
+                </div>
+            `;
+            productsList.appendChild(productItem);
+        }
+    } catch (error) {
+        console.error(error);
+        productsList.innerHTML = '<p>Ошибка загрузки товаров. Попробуйте позже.</p>';
+    }
+
+    initializeCarousel();
+}
 
 document.addEventListener('DOMContentLoaded', () => {
     const preloader = document.getElementById('preloader');
     const mainContent = document.getElementById('main-content');
 
-    async function loadCategories() {
-        const categoryList = document.getElementById('category-list');
-        try {
-            const response = await fetch('/api/v1/edit-shop/get-all/categories/');
-            const categories = await response.json();
-
-            categoryList.innerHTML = ''; // Очищаем список перед загрузкой
-            if (categories.length === 0) {
-                categoryList.innerHTML = '<p>Категории появятся совсем скоро =)</p>';
-                return;
-            }
-
-            categories.forEach(category => {
-                const categoryItem = document.createElement('div');
-                categoryItem.classList.add('category-card'); // Класс для стилизации карточки
-
-                categoryItem.innerHTML = `
-                <div class="category-value">
-                    <a href="/main/shop/category-page/${category.id}">${category.category}</a>
-                </div>
-            `;
-                categoryList.appendChild(categoryItem);
-            });
-        } catch (error) {
-            console.error(error);
-            categoryList.innerHTML = '<p>Ошибка загрузки категорий. Попробуйте позже.</p>';
-        }
-    }
-
     loadCategories();
+    loadProducts();
 
     setTimeout(() => {
         preloader.classList.add('hidden');
